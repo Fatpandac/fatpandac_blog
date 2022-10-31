93dd8d9e7e619ab814665f85888fb869d9d6660d
Fatpandac <i@fatpandac.com>
2022-10-31 21:42:58 +0800

    chore: move blog to deno

---
+++ main.tsx
@@ -0,0 +1,17 @@
+import blog, { ga, redirects, h } from "blog";
+
+blog({
+  title: "Fatpandac",
+  description: "Coach, I want to learn this.",
+  avatar: "/images/avatar.png",
+  avatarClass: "rounded-full",
+  author: "Fatpandac",
+  links: [
+    { title: "Email", url: "mailto:i@fatpandac.com" },
+    { title: "GitHub", url: "https://github.com/Fatpandac" },
+    { title: "Twitter", url: "https://twitter.com/Fatpandac" },
+    { title: "Telegram", url: "https://t.me/fatpandac" },
+  ],
+  favicon: "/images/logo.png"
+});
+
