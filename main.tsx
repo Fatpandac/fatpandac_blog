654a41e7452eb7cc14d40d2a289eea125a6bde33
Fatpandac <i@fatpandac.com>
2022-11-01 14:12:13 +0800

    feat: setup analytics

--- main.tsx
+++ main.tsx
@@ -1,4 +1,4 @@
-import blog, { ga, redirects, h } from "blog";
+import blog, { redirects } from "blog";
 
 blog({
   title: "Fatpandac",
@@ -6,6 +6,32 @@ blog({
   avatar: "/images/avatar.png",
   avatarClass: "rounded-full",
   author: "Fatpandac",
+  scripts: [
+    `
+    (function() {
+      var ga = document.createElement('script');
+      ga.src = 'https://www.googletagmanager.com/gtag/js?id=G-10D6YDNQ9J';
+      ga.async = true;
+      document.head.appendChild(ga);
+    })();
+    `,
+    `
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+  
+    gtag('config', 'G-10D6YDNQ9J');
+    `,
+    `
+    var _hmt = _hmt || [];
+    (function() {
+      var hm = document.createElement("script");
+      hm.src = "https://hm.baidu.com/hm.js?d47c012aa2838250165b44d65f898c3b";
+      var s = document.getElementsByTagName("script")[0]; 
+      s.parentNode.insertBefore(hm, s);
+    })();
+    `,
+  ],
   links: [
     { title: "Email", url: "mailto:i@fatpandac.com" },
     { title: "GitHub", url: "https://github.com/Fatpandac" },
@@ -14,7 +40,6 @@ blog({
   ],
   favicon: "/images/logo.png",
   middlewares: [
-    ga("G-10D6YDNQ9J"),
     redirects({
       "/docs/other": "",
       ".html": "",
